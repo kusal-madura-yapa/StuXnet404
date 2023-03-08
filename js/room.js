f19146fd39cb5128ffe19b83f614938847d7c39a
Kusal Madura Yapa <kusalmadurayapa@Kusals-MacBook-Pro.local>
2023-03-08 18:12:22 +0530

    cahnge index path

--- js/room.js
+++ js/room.js
@@ -32,9 +32,13 @@ chatButton.addEventListener('click', () => {
 
 });
 
+// get the display frame of the video
 let displayFrame = document.getElementById('stream__box')
+// get the all video frames container in the stream
 let videoFrames = document.getElementsByClassName('video__container')
+// dsplaya fraim user id 
 let userIdInDisplayFrame = null;
+
 
 let expandVideoFrame = (e) => {
 
@@ -42,12 +46,15 @@ let expandVideoFrame = (e) => {
   if(child){
       document.getElementById('streams__container').appendChild(child)
   }
-
+// display the frame and connect the video to it
   displayFrame.style.display = 'block'
   displayFrame.appendChild(e.currentTarget)
+  // get the current user id
   userIdInDisplayFrame = e.currentTarget.id
 
+  // handle the vedio frames size and the display frame size and the number of vedio frames 
   for(let i = 0; videoFrames.length > i; i++){
+    // current ID != user id in display frame then change the height and width of the video frame
     if(videoFrames[i].id != userIdInDisplayFrame){
       videoFrames[i].style.height = '100px'
       videoFrames[i].style.width = '100px'
@@ -56,6 +63,9 @@ let expandVideoFrame = (e) => {
 
 }
 
+
+// if the user click on the display frame hide it and show the video frames
+// but user face can't this is for the screen share 
 for(let i = 0; videoFrames.length > i; i++){
   videoFrames[i].addEventListener('click', expandVideoFrame)
 }
@@ -65,7 +75,10 @@ let hideDisplayFrame = () => {
     userIdInDisplayFrame = null
     displayFrame.style.display = null
 
+
+    // get the child of the display frame and append it to the stream container
     let child = displayFrame.children[0]
+    // if the child is not null append it to the stream container
     document.getElementById('streams__container').appendChild(child)
 
     for(let i = 0; videoFrames.length > i; i++){
