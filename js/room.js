36b22efbb129d8c7fd0d4c38cbf2bfacc282cc5c
Kusal Madura Yapa <kusalmadurayapa@Kusals-MacBook-Pro.local>
2023-03-08 15:37:50 +0530

    add the multiple meeting platform

--- js/room.js
+++ js/room.js
@@ -30,3 +30,47 @@ chatButton.addEventListener('click', () => {
 
   activeChatContainer = !activeChatContainer;
 });
+
+let displayFrame = document.getElementById('stream__box')
+let videoFrames = document.getElementsByClassName('video__container')
+let userIdInDisplayFrame = null;
+
+let expandVideoFrame = (e) => {
+
+  let child = displayFrame.children[0]
+  if(child){
+      document.getElementById('streams__container').appendChild(child)
+  }
+
+  displayFrame.style.display = 'block'
+  displayFrame.appendChild(e.currentTarget)
+  userIdInDisplayFrame = e.currentTarget.id
+
+  for(let i = 0; videoFrames.length > i; i++){
+    if(videoFrames[i].id != userIdInDisplayFrame){
+      videoFrames[i].style.height = '100px'
+      videoFrames[i].style.width = '100px'
+    }
+  }
+
+}
+
+for(let i = 0; videoFrames.length > i; i++){
+  videoFrames[i].addEventListener('click', expandVideoFrame)
+}
+
+
+let hideDisplayFrame = () => {
+    userIdInDisplayFrame = null
+    displayFrame.style.display = null
+
+    let child = displayFrame.children[0]
+    document.getElementById('streams__container').appendChild(child)
+
+    for(let i = 0; videoFrames.length > i; i++){
+      videoFrames[i].style.height = '300px'
+      videoFrames[i].style.width = '300px'
+  }
+}
+
+displayFrame.addEventListener('click', hideDisplayFrame)
