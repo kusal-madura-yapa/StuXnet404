c742be19c771b9d6029fc181d25e4333668563f5
Pasindu Hasaranga <98208099+Voidwalker1999@users.noreply.github.com>
2023-04-01 19:21:32 +0530

    Add files via upload

---
+++ build/precache-manifest.6d794fd0b7b14f9da6b4fa5b054709a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7184b3d31c9c477cdbf6a576ea3728",
+    "url": "/index.html"
+  },
+  {
+    "revision": "824da9417e54a80bd933",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "1d04fbfc4f5bde6626e8",
+    "url": "/static/js/2.0d83935f.chunk.js"
+  },
+  {
+    "revision": "83d508e65579b4b814929af9a1619e62",
+    "url": "/static/js/2.0d83935f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "824da9417e54a80bd933",
+    "url": "/static/js/main.91ee0dba.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.91ee0dba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd4a854c06bb36be3a7d",
+    "url": "/static/js/runtime-main.1530841d.js"
+  }
+]);
